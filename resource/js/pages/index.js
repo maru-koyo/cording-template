e572a61355f73293f7c7f28730e5a17847caf983
k.takaya <k.takaya@marukinad.co.jp>
2023-11-29 09:07:13 +0900

    ｎ

--- resource/js/pages/index.js
+++ resource/js/pages/index.js
@@ -3,7 +3,6 @@ if (data.indexOf("-") !== -1) {
   data = data.replace(/-/g, "/");
 }
 const dir = `/${data}/`;
-
 export default function Pages() {
   import(`.${dir}`).then((module) => {
     module.default();
